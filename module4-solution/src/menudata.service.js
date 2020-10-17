27af86c2a817ea88f4fe89d1030ed302454ecd27
Jeffrey Hanssen <fitbuildjeff@gmail.com>
2020-10-17 08:03:58 -0700

    fixed final error

--- module4-solution/src/menudata.service.js
+++ module4-solution/src/menudata.service.js
@@ -1,21 +1,25 @@
 (function() {
 'use strict';
-    
+        
 angular.module('data')
 .service('MenuDataService', MenuDataService)
 .constant('apiBasePath', 'https://davids-restaurant.herokuapp.com');
-    
+        
 MenuDataService.$inject = ['$http', 'apiBasePath']
 function MenuDataService($http, apiBasePath) {
   var service = this;
-    
+        
   service.getAllCategories = function() {
     return $http.get(apiBasePath + '/categories.json');
   }
-
+    
   service.getItemsForCategory = function(categoryShortName) {
-    return $http.get(apiBasePath + "/menu_items.json?category" + categoryShortName);
+    return $http({
+      method: 'GET',
+      url: apiBasePath + '/menu_items.json',
+      params: {category: categoryShortName}
+    });
   }
-
+    
 }
 })();
